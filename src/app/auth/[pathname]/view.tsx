79cde8c4590e1d2ad25339df2b6cc85f7b54fb31
ArvindParekh <aruparekh@gmail.com>
2025-06-29 14:06:35 -0400

    feat(auth): add toaster to all signin screens for feedback

--- src/app/auth/[pathname]/view.tsx
+++ src/app/auth/[pathname]/view.tsx
@@ -1,11 +1,13 @@
 "use client"
 
+import { Toaster } from "@/components/ui/sonner"
 import { AuthCard } from "@daveyplate/better-auth-ui"
 
 export function AuthView({ pathname }: { pathname: string }) {
     return (
         <main className="container flex grow flex-col items-center justify-center gap-3 self-center p-4 md:p-6 w-full h-screen mx-auto">
             <AuthCard pathname={pathname} />
+            <Toaster position="top-center" richColors={true} />
         </main>
     )
 }
